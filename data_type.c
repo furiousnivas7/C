d231e0522c149556cc81558ac28ac3f40916ce10
Nivash_sharma <nivasmathu@gmail.com>
2024-05-07 20:28:54 +0530

    data type install

---
+++ data_type.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    int myNum=50;
+    printf(myNum);
+    return 0;
+}
--- helloworld.c
+++ helloworld.c
@@ -2,6 +2,7 @@
 
 int main(){
     printf("hello world");
+    printf("have a good day");
     return 0;
 }
 
